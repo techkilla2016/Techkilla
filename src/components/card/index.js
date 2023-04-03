63e266366c6247cd7992199d5f67cc8794cfd842
w3App <kshatriyakabawat@gmail.com>
2023-04-03 22:09:48 +0530

    fix image

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
 import Button from '../button/Button'
-import Image from 'next/image'
 const Card = ({ img, title, description, slag, color }) => {
     return (
         <Col xxl={3} xl={3} lg={4} md={6} sm={6} className='my-3'>
             <div className="serviceCart">
                 <div className="iconContainer" style={{ background: color }}>
                     <div className="serviceIcon">
-                        <Image src={img} />
+                        <img src={img} />
                     </div>
                 </div>
                 <div className="cardDesc">
