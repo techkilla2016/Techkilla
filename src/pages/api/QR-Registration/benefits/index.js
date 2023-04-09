66337a4ac034eee8054b7dcf961c3f86b8ba3387
w3App <kshatriyakabawat@gmail.com>
2023-04-09 12:20:03 +0530

    /assets/microsite into /microsite

--- src/pages/api/QR-Registration/benefits/index.js
+++ src/pages/api/QR-Registration/benefits/index.js
@@ -1,26 +1,26 @@
 const benefits = [
     {
-        img: '/assets/microsite/microsite-b-01.png',
+        img: '/microsite/microsite-b-01.png',
         title: `Chaos-free Audience Management`
     },
     {
-        img: '/assets/microsite/microsite-b-02.png',
+        img: '/microsite/microsite-b-02.png',
         title: `Effortless`
     },
     {
-        img: '/assets/microsite/microsite-b-03.png',
+        img: '/microsite/microsite-b-03.png',
         title: `100% Backend Record`
     },
     {
-        img: '/assets/microsite/microsite-b-04.png',
+        img: '/microsite/microsite-b-04.png',
         title: `Integrity of the data`
     },
     {
-        img: '/assets/microsite/microsite-b-05.png',
+        img: '/microsite/microsite-b-05.png',
         title: `Auto-email`
     },
     {
-        img: '/assets/microsite/microsite-b-06.png',
+        img: '/microsite/microsite-b-06.png',
         title: `Time-saver`
     },
 ]
