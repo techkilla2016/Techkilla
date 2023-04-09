33bca6a53f52ab0d5ecd5d2c83f299b6043877eb
w3App <kshatriyakabawat@gmail.com>
2023-04-09 12:00:24 +0530

    service page

--- src/components/home/section4/CardSlider.js
+++ src/components/home/section4/CardSlider.js
@@ -4,9 +4,59 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Autoplay, Pagination, Navigation } from 'swiper';
 import { BsArrowRight } from 'react-icons/bs'
-import cartList from "./cartData";
 import Link from "next/link";
 import Image from "next/image";
+const cartList = [
+    {
+        _id: 1,
+        tag: 'IPL',
+        img: '/home/thumbnails01.webp',
+        target: '/',
+        title: 'Instagram AR Filter'
+    },
+    {
+        _id: 2,
+        tag: 'KUKU FM',
+        img: '/home/thumbnails02.webp',
+        target: '/',
+        title: 'Instagram AR Filter'
+    },
+    {
+        _id: 3,
+        tag: 'KUKU FM',
+        img: '/home/thumbnails03.webp',
+        target: '/',
+        title: 'Facebook Playable Ad-'
+    },
+    {
+        _id: 4,
+        tag: 'MIROSOFT PRIDE',
+        img: '/home/thumbnails04.webp',
+        target: '/',
+        title: 'Instagram AR Filter'
+    },
+    {
+        _id: 5,
+        tag: 'KUKU FM',
+        img: '/home/thumbnails05.webp',
+        target: '/',
+        title: 'Instagram AR Filter'
+    },
+    {
+        _id: 6,
+        tag: 'CEAT TYRES',
+        img: '/home/thumbnails06.webp',
+        target: '/',
+        title: 'VR Experience'
+    },
+    {
+        _id: 7,
+        tag: 'TINDER',
+        img: '/home/thumbnails07.webp',
+        target: '/',
+        title: 'Instagram AR Filter'
+    },
+]
 export default function CardSlider() {
     return (
 
@@ -45,13 +95,13 @@ export default function CardSlider() {
                         return (
                             <SwiperSlide key={curCart._id}>
                                 <div className="cartContainer">
-                                    <Image src={curCart.img} alt="" width={100} height={100}/>
+                                    <Image src={curCart.img} alt={curCart.title} width={100} height={100} />
                                     <div className="cartContant">
                                         <div className="tag">{curCart.tag}</div>
                                         <div className="portfolio__title">{curCart.title}</div>
                                         <div className="portfolioCartBtn">
                                             <Link href={curCart.target}><BsArrowRight /></Link>
-                                        </div>  
+                                        </div>
                                     </div>
                                 </div>
                             </SwiperSlide>
