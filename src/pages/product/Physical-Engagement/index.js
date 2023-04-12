926ec4ea4ec6a90b8e789094355a1e64516b32f0
w3App <kshatriyakabawat@gmail.com>
2023-04-12 17:02:41 +0530

    physical engagement

--- src/pages/product/Physical-Engagement/index.js
+++ src/pages/product/Physical-Engagement/index.js
@@ -7,10 +7,15 @@ import React from 'react'
 import { useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 const PhysicalEngagement = ({ product }) => {
-    const [isPreloader, setIsPreloader] = useState(true)
-    setTimeout(() => {
-        setIsPreloader(false)
-    }, 3090)
+    const [isPlay, setIsPlay] = useState(false)
+    const [curVideo, setCurVideo] = useState('')
+    const handleClose = () => {
+        setIsPlay(false)
+    }
+    const handleOpen = (payload) => {
+        setIsPlay(true)
+        setCurVideo(payload)
+    }
     return (
         <>
             <Head>
@@ -23,20 +28,37 @@ const PhysicalEngagement = ({ product }) => {
                         <div className='product_landing_background'>
                             <div className='preloaderBack'>
                                 <Image src='/product-01.png' width={1920} height={1080} />
+                                <Image src='/product-01_01.png' width={1920} height={1080} />
+                                <div className='preloaderBack-layer'>
+                                </div>
                             </div>
-                        </div>
-                        <div className='product-home'>
-                            <Container>
-                                <p>
-                                    In-person engagement is an essential part of building strong customer relationships, and experiential technology products can help businesses create memorable experiences that connect with their audiences.
-                                </p>
-                                <p>
-                                    Our physical engagement experiential technology products are designed to help businesses create immersive and memorable experiences that connect with their customers on a deeper level.
-                                </p>
-                            </Container>
+                            <div className='product_intro'>
+                                <Container>
+                                    <Row>
+                                        <Col xxl={8} xl={8} lg={8} md={8} sm={12} xm={12}>
+                                            <div className='product_main'>
+                                                <h2>
+                                                    Drive engagement with digital innovation
+                                                </h2>
+                                                <p>
+                                                    In-person engagement is an essential part of building strong customer relationships, and experiential technology products can help businesses create memorable experiences that connect with their audiences.
+                                                </p>
+                                            </div>
+                                        </Col>
+                                        <Col xxl={4} xl={4} lg={4} md={4} sm={0} xm={0}>
+                                            <div className='product_main_icon'>
+                                                <Image src='/vrr.png' width={1080} height={1920} />
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </div>
                         </div>
                     </div>
                     <div className='product-container'>
+                        {/* <h2>
+                            Our physical engagement experiential technology products are designed to help businesses create immersive and memorable experiences that connect with their customers on a deeper level.
+                        </h2> */}
                         <Container>
                             <div className='Heading'>
                                 Product
@@ -45,8 +67,8 @@ const PhysicalEngagement = ({ product }) => {
                                 {
                                     product?.map((curItem, keys) => {
                                         return (
-                                            <Col key={keys} xxl={3} xl={3} lg={4} md={6} sm={6} xm={12} className='my-4'>
-                                                <ProdcutCard {...curItem} />
+                                            <Col key={keys} xxl={4} xl={4} lg={4} md={6} sm={6} xm={12} className='my-4'>
+                                                <ProdcutCard {...curItem} handleClick={handleOpen} />
                                             </Col>
                                         )
                                     })
@@ -54,6 +76,10 @@ const PhysicalEngagement = ({ product }) => {
                             </Row>
                         </Container>
                     </div>
+
+                    {
+                        isPlay && <YouTubePlayer url={curVideo} handleClose={handleClose} />
+                    }
                     <div className='mainContainer my-5'>
                         <Footer />
                     </div>
@@ -63,6 +89,8 @@ const PhysicalEngagement = ({ product }) => {
         </>
     )
 }
+
+
 export default PhysicalEngagement
 export const getServerSideProps = async () => {
     const res = await fetch('https://techkilla.vercel.app/api/product/physical-engagement/')
