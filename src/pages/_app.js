5562e3570e7ac1834e2342bc13bca6e3e0d6f3b4
w3App <kshatriyakabawat@gmail.com>
2023-05-19 12:32:43 +0530

    google's code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Move the dataLayer initialization here
+    window.dataLayer = window.dataLayer || [];
+
+    // Call the gtag function after initialization
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-TV1ZCTF238');
+  }, []); // Empty dependency array ensures this runs only once on mount
+
   return (
     <>
       <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-252119773-1"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments)}
-              gtag('js', new Date());
-              gtag('config', 'UA-252119773-1');
-            `,
-          }}
-        ></script>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TV1ZCTF238"></script>
       </Head>
       <Component {...pageProps} />
     </>
