e8f738013ef55b712d4ad73bed07831a2cdded17
w3App <kshatriyakabawat@gmail.com>
2023-05-22 12:57:53 +0530

    change g Id

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,18 +4,38 @@ import Head from 'next/head'
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments) }
-    gtag('js', new Date());
-    gtag('config', 'G-1Q8D92YRHE');
-  }, []);
+  // useEffect(() => {
+  //   // Move the dataLayer initialization here
+  //   window.dataLayer = window.dataLayer || [];
+
+  //   // Call the gtag function after initialization
+  //   function gtag() {
+  //     window.dataLayer.push(arguments);
+  //   }
+  //   gtag('js', new Date());
+  //   gtag('config', 'UA-252119773-1');
+  // }, []); // Empty dependency array ensures this runs only once on mount
 
   return (
     <>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1Q8D92YRHE"></script>
         {/* <!-- Google tag (gtag.js) --> */}
+        {/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-252119773-1"></script> */}
+        <script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=UA-252119773-1" />
+        <script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-252119773-1', {
+ page_path: window.location.pathname,
+ });
+`,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
