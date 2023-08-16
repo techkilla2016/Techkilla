a1ebfeb50449dbedd7ece71ebea28dc8cff7532b
w3App <kshatriyakabawat@gmail.com>
2023-08-16 14:06:19 +0530

    fix header

--- src/pages/Qr-Solution/index.js
+++ src/pages/Qr-Solution/index.js
@@ -3,6 +3,8 @@ import { Col, Container, Row, } from 'react-bootstrap'
 import QrForm from '@/components/contect/FormQR'
 import Image from 'next/image'
 import Footer01 from '@/components/footer/footer_01'
+import { HiOutlineMail } from 'react-icons/hi'
+import { GiSmartphone } from 'react-icons/gi'
 const funData = [
     {
         title: 'Form to sign up for an event or campaign',
@@ -157,18 +159,22 @@ const App = () => {
     return (
         <>
             <div className="main">
-                <div className='d-flex justify-content-between align-items-center py-1 px-3 qr-header flex-wrap'>
-                    <div className="qr-logo">
-                        <Image src="/logo/logo.png" width={70} height={70} />
-                    </div>
-                    <div className="d-flex flex-wrap">
-                        <div className='text-dark'>
-                            <strong>Email</strong> : example@gmail.com
+                <div className='row align-items-center py-1 px-3 qr-header'>
+                    <Col xxl={2} xl={2} lg={2} md={2} sm={2} xs={2}>
+                        <div className="qr-logo">
+                            <Image src="/logo/logo.png" width={70} height={70} />
                         </div>
-                        <div className='px-4 text-dark'>
-                            <strong>Phone</strong> : +91 9878457645
+                    </Col>
+                    <Col xxl={10} xl={10} lg={10} md={10} sm={10} xs={10}>
+                        <div className="d-flex justify-content-end flex-wrap">
+                            <div className='text-dark context-id ' >
+                                <strong><HiOutlineMail /></strong> example@gmail.com
+                            </div>
+                            <div className='text-dark context-id' id='context-id'>
+                                <strong><GiSmartphone /></strong> +91 9878457645
+                            </div>
                         </div>
-                    </div>
+                    </Col>
                 </div>
                 <div className="mainContainer">
                     <div className="contact container-fluid">
@@ -346,7 +352,7 @@ const App = () => {
                     </div>
                 </div>
             </div>
-                <Footer01/>
+            <Footer01 />
             {/* <div className="qr-footer bg-dark py-5">
             </div> */}
         </>
