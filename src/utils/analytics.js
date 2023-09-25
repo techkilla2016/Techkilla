e05bffc43567449b5076c6204ef592a44a4419f2
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-09-25 16:00:37 +0530

    test

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -4,6 +4,7 @@ export const initGA = () => {
     ReactGA.initialize('G-1Q8D92YRHE');
 };
 
+
 export const logPageView = () => {
     ReactGA.set({ page: window.location.pathname });
     ReactGA.pageview(window.location.pathname);
