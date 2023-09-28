735bff7a9192ab95af373d397d28796f11ddb607
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-09-28 11:34:02 +0530

    responsive

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -17,41 +17,72 @@ const Testimonials = ({ cardData }) => {
         setMaxHeight(maxCardHeight);
     }, [cardData]);
     return (
-        <Swiper
-            // modules={[Virtual]}
-            slidesPerView={1.6}
-            spaceBetween={50}
-            centeredSlides={true}
-            className="mySwiper py-5"
-            autoplay={{
-                delay: 2000,
-                disableOnInteraction: false,
-            }}
-            loop={true}
-        // modules={[Autoplay]}
-        >
-            {cardData.map((item, index) => (
-                <SwiperSlide key={index} virtualIndex={index}>
-                    <div className="slider-ai-box d-flex flex-column justify-content-center text-start">
-                        <p className='text-secondary py-4' >
-                            {index + 1}/{cardData?.length}
-                        </p>
-                        <p className='text-secondary' >
-                            {item?.desc}
-                        </p>
-                        <h3 className='text-secondary'>
-                            <strong>
-                                {item?.name}
-                            </strong>
-                        </h3>
-                        <p className='text-secondary'>
-                            <strong> {item?.post}</strong> <br />
-                            {item?.company}
-                        </p>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+
+        <>
+            <div className="swiperOne">
+                <Swiper navigation={false} className="mySwiper">
+                    {cardData.map((item, index) => (
+                        <SwiperSlide key={index} virtualIndex={index}>
+                            <div className="slider-ai-box d-flex flex-column justify-content-center text-start">
+                                <p className='text-secondary py-4' >
+                                    {index + 1}/{cardData?.length}
+                                </p>
+                                <p className='text-secondary' >
+                                    {item?.desc}
+                                </p>
+                                <h3 className='text-secondary'>
+                                    <strong>
+                                        {item?.name}
+                                    </strong>
+                                </h3>
+                                <p className='text-secondary'>
+                                    <strong> {item?.post}</strong> <br />
+                                    {item?.company}
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="swiperTwo">
+                <Swiper
+                    // modules={[Virtual]}
+                    slidesPerView={1.6}
+                    spaceBetween={50}
+                    centeredSlides={true}
+                    className="mySwiper py-5"
+                    autoplay={{
+                        delay: 2000,
+                        disableOnInteraction: false,
+                    }}
+                    loop={true}
+                // modules={[Autoplay]}
+                >
+                    {cardData.map((item, index) => (
+                        <SwiperSlide key={index} virtualIndex={index}>
+                            <div className="slider-ai-box d-flex flex-column justify-content-center text-start">
+                                <p className='text-secondary py-4' >
+                                    {index + 1}/{cardData?.length}
+                                </p>
+                                <p className='text-secondary' >
+                                    {item?.desc}
+                                </p>
+                                <h3 className='text-secondary'>
+                                    <strong>
+                                        {item?.name}
+                                    </strong>
+                                </h3>
+                                <p className='text-secondary'>
+                                    <strong> {item?.post}</strong> <br />
+                                    {item?.company}
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+
+        </>
     )
 }
 
