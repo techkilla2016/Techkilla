70a75b1ec7e1d4015348376f302662038a2c37a8
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2023-10-28 11:50:19 +0530

    add google ads code

--- src/pages/Qr-Solution/index.js
+++ src/pages/Qr-Solution/index.js
@@ -8,9 +8,6 @@ import { GiSmartphone } from 'react-icons/gi'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-import auth from '@/models/auth'
-import Head from 'next/head'
-import Script from 'next/script'
 const funData = [
     {
         title: 'Form to sign up for an event or campaign',
@@ -77,58 +74,6 @@ const benefits = [
         title: `Time-saver`
     },
 ]
-
-const userCase = [
-    {
-        icon: '/microsite/user-case01.png',
-        title: 'Pre event registration',
-        desc: `Online forms are increasingly being used for pre-event registration, when attendees provide their contact information and indicate whether they want to attend the event virtually or in person. Techkilla allows you to give your form a look and feel that is consistent with your brand.`
-    },
-
-    {
-        icon: '/microsite/user-case02.png',
-        title: 'On spot registration',
-        desc: `On-the-spot registration is performed at any event that requests attendee 
-        information and requires immediate identification. For instance, at the 
-        registration counter at trade shows and fairs, attendees' personal data is 
-        collected and an ID card is promptly printed`
-    },
-
-    {
-        icon: '/microsite/user-case03.png',
-        title: 'Entry verification',
-        desc: `The verification of registered users' entries plays a crucial part in crowd 
-        control by allowing entrance only to confirmed attendance. Techkilla's 
-        web-based scanner programme reads the QR code to verify the user's 
-        identity.`
-    },
-
-    {
-        icon: '/microsite/user-case04.png',
-        title: 'Audience management',
-        desc: `When working with a large group of people, it might be difficult to 
-        keep everyone under control. Our proprietary software streamlines
-        the process of managing a large audience by handling tasks such
-        as signup, automated emailing, QR code production, verification, 
-        and database administration.`
-    },
-
-    {
-        icon: '/microsite/user-case05.webp',
-        title: 'Student registration',
-        desc: `It is during admissions, college festivals, and other cultural events that 
-        the majority of students sign up to attend. Our AI technology is able to 
-        allow the management to operate on registration and fast ID printing 
-        within no time and seamlessly.`
-    },
-    {
-        icon: '/microsite/user-case06.webp',
-        title: 'Goodies distribution and many more',
-        desc: `Almost every event and freebie distribution includes the delivery of 
-        goodies as a form of appreciation. Get the user database with the help 
-        of our software and send out rewards to real people.`
-    },
-]
 const choose_list = [
     {
         title: 'Cost-effective solutions',
@@ -183,40 +128,15 @@ const App = () => {
 
     return (
         <>
-            <Head>
-                <Script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PVH2G66V');
-            `,
-                    }}
-                />
-                <Script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PVH2G66V');
-            `,
-                    }}
-                />
-                {/* Add the noscript part */}
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-PVH2G66V"
-                        height="0"
-                        width="0"
-                        style={{ display: 'none', visibility: 'hidden' }}
-                    ></iframe>
-                </noscript>
-            </Head>
             <div className="main" id='demo'>
+
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-PVH2G66V"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                ></iframe>
+
                 <div className='row align-items-center py-1 px-3 qr-header'>
                     <Col xxl={2} xl={2} lg={2} md={2} sm={2} xs={2}>
                         <div className="qr-logo">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,27 +30,23 @@ export default function App({ Component, pageProps }) {
                 `,
           }}
         />
-        {/* Add the Google Tag Manager script */}
-        <Script
+
+        {/* Other head elements */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-PVH2G66V');
             `,
           }}
         />
-        {/* Add the noscript part */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PVH2G66V"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
       </Head>
       <Component {...pageProps} />
     </>
