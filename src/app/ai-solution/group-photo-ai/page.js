e6dca5f0ba4e1ddf29cef256e9e190cbac120ed7
Mukesh Singh <118162167+kabawat@users.noreply.github.com>
2024-01-02 11:18:56 +0530

    change Image

--- src/app/ai-solution/group-photo-ai/page.js
+++ src/app/ai-solution/group-photo-ai/page.js
@@ -105,12 +105,12 @@ const Home = () => {
                                 <div className="d-flex justify-content-center" data-aos="zoom-in-up" data-aos-delay="0">
                                     {/* <SwipeCard /> */}
                                     <Card>
-                                        <img src='/assets/tk-ai/gif/Avengers.gif' className='rounded' />
+                                        <img src='/assets/tk-ai/gif/Archies.gif' className='rounded' />
                                     </Card>
                                 </div>
                             </Col>
                             <Col xxl={6} xl={6} lg={6} md={6} sm={12} xs={12} className='tk-ai-col px-5'>
-                                <h3 data-aos="fade-up" data-aos-delay="100" className='fw-bold'>For Media & Promotional Purposes</h3>
+                                <h2 data-aos="fade-up" data-aos-delay="100" className='fw-bold'>For Media & Promotional Purposes</h2>
                                 <p data-aos="fade-up" data-aos-delay="300" className='ai-title'>
                                     Generate social media excitement or captivate the audience by allowing them to morph into their beloved movie characters, just like in the renowned Netflix film "Archies."
                                 </p>
