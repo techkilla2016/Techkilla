3891b7e1500f7adcbdc343a37417d3c9c57b8a84
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-29 17:35:33 +0530

    whatsapp modal 1 minute expire cookie used, whatsapp button animation, hero section's ai photobooth text change, continue button's routing update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,34 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "swiper/css/effect-coverflow";
 import "react-phone-input-2/lib/style.css";
-import 'aos/dist/aos.css';
-import './style.scss'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-import App from './app'
+import "aos/dist/aos.css";
+import "./style.scss";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+import App from "./app";
+import Link from "next/link";
+import { BsWhatsapp } from "react-icons/bs";
+
 export const metadata = {
-  title: 'Techkilla',
+  title: "Techkilla",
   description: `With Techkilla's innovative experiential technology solutions, businesses can enhance their customer engagement and increase brand loyalty. Contact us today to learn more about how we can help you achieve your business goals.`,
-}
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <App />
         {children}
+        <Link
+          className="whatsapp-btn"
+          href="http://wa.me/917827362702?text=Hi Techkilla Team,%0AI am looking for some tech solutions. %0AKindly revert on this%0A%0AThanks."
+        >
+          <BsWhatsapp />
+        </Link>
       </body>
     </html>
-  )
+  );
 }
--- src/components/button/scrollBtn.js
+++ src/components/button/scrollBtn.js
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import React from 'react'
-import { BsArrowRight } from 'react-icons/bs'
+import Link from "next/link";
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 const ScrollBtn = () => {
-    return (
-        <Link href='/ai-solution/single-photo-ai/' className='scroll-btn' >
-            <span>Continue</span>
-            <BsArrowRight />
-        </Link>
-    )
-}
-export default ScrollBtn
+  return (
+    <Link href="/ai-photobooth" className="scroll-btn">
+      <span>Continue</span>
+      <BsArrowRight />
+    </Link>
+  );
+};
+export default ScrollBtn;
