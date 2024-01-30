13cae088cf95112d149bf9cb372165fa83f9b6cc
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-01-30 17:48:25 +0530

    Book a free demo not working issue solved

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,119 +1,288 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Button = ({ url, target, title, openWith }) => {
-    return (
-        <Link href={url} target={target ? target : openWith} className='startButton'>{title}
-            <div className="star-1">
-                <svg viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-            </div>
-            <div className="star-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }} version="1.1" xmlSpace="preserve">
-                    <defs></defs>
-                    <g id="Layer_x0020_1">
-                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                        <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path>
-                    </g>
-                </svg>
-            </div>
-            <div className="star-3">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <g id="Layer_x0020_1">
-                        <path className="fil0" d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" />
-                    </g>
-                </svg>
-            </div>
-            <div className="star-4">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53" style={{ shapeRendering: "geometricPrecision", textRendering: "geometricPrecision", imageRendering: "optimizeQuality", fillRule: "evenodd", clipRule: "evenodd" }}>
-                    <defs></defs>
-                    <g id="Layer_x0020_1">
-                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                        <path className="fil0" d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" />
-                    </g>
-                </svg>
-
-            </div>
-            <div className="star-5">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-            </div>
-            <div className="star-6">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link
+      href={url}
+      target={target ? target : openWith}
+      className="startButton"
+    >
+      {title}
+      <div className="star-1">
+        <svg
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+      <div className="star-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+          version="1.1"
+          xmlSpace="preserve"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+              className="fil0"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <g id="Layer_x0020_1">
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="star-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="star-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+      <div className="star-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+    </Link>
+  );
+};
 
 export const ButtonClick = ({ handleClick, title, target }) => {
-    return (
-        <button onClick={() => handleClick(target)} className='startButton'>{title}
-            <div className="star-1">
-                <svg viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-
-            </div>
-            <div className="star-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }} version="1.1" xmlSpace="preserve">
-                    <defs></defs>
-                    <g id="Layer_x0020_1">
-                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                        <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0"></path>
-                    </g>
-                </svg>
-
-            </div>
-            <div className="star-3">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <g id="Layer_x0020_1">
-                        <path className="fil0" d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" />
-                    </g>
-                </svg>
-
-            </div>
-            <div className="star-4">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 784.11 815.53" style={{ shapeRendering: "geometricPrecision", textRendering: "geometricPrecision", imageRendering: "optimizeQuality", fillRule: "evenodd", clipRule: "evenodd" }}>
-                    <defs></defs>
-                    <g id="Layer_x0020_1">
-                        <metadata id="CorelCorpID_0Corel-Layer"></metadata>
-                        <path className="fil0" d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" />
-                    </g>
-                </svg>
-
-            </div>
-            <div className="star-5">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-            </div>
-            <div className="star-6">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 784.11 815.53" style={{ shapeRendering: 'geometricPrecision', textRendering: 'geometricPrecision', imageRendering: 'optimizeQuality', fillRule: 'evenodd', clipRule: 'evenodd' }}>
-                    <path d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z" className="fil0" />
-                </svg>
-            </div>
-        </button>
-    )
-}
+  return (
+    <button onClick={() => handleClick(target)} className="startButton">
+      {title}
+      <div className="star-1">
+        <svg
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+      <div className="star-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+          version="1.1"
+          xmlSpace="preserve"
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+              className="fil0"
+            ></path>
+          </g>
+        </svg>
+      </div>
+      <div className="star-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <g id="Layer_x0020_1">
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="star-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <defs></defs>
+          <g id="Layer_x0020_1">
+            <metadata id="CorelCorpID_0Corel-Layer"></metadata>
+            <path
+              className="fil0"
+              d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            />
+          </g>
+        </svg>
+      </div>
+      <div className="star-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+      <div className="star-6">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 784.11 815.53"
+          style={{
+            shapeRendering: "geometricPrecision",
+            textRendering: "geometricPrecision",
+            imageRendering: "optimizeQuality",
+            fillRule: "evenodd",
+            clipRule: "evenodd",
+          }}
+        >
+          <path
+            d="M392.05 0c-20.9,210.08 -184.06,378.41 -392.05,407.78 207.96,29.37 371.12,197.68 392.05,407.74 20.93,-210.06 184.09,-378.37 392.05,-407.74 -207.98,-29.38 -371.16,-197.69 -392.06,-407.78z"
+            className="fil0"
+          />
+        </svg>
+      </div>
+    </button>
+  );
+};
 
 export const ButtonLight = ({ target, title, type }) => {
-    return (
-        target ? <button role="button" className="button2">{title}</button>
-            : <Link href={target ? target : '/'} target='_blank'>
-                <button role="button" className="button2">{title}</button>
-            </Link>
-    )
-}
+  return target ? (
+    <Link href={target ? target : "/"} target="_blank">
+      <button role="button" className="button2">
+        {title}
+      </button>
+    </Link>
+  ) : (
+    <Link href={target ? target : "/"} target="_blank">
+      <button role="button" className="button2">
+        {title}
+      </button>
+    </Link>
+  );
+};
 
 export const ButtonSwap = ({ clickHandel, url, title }) => {
-    return <button className='buttonSwap' onClick={() => clickHandel(url)}>
-        <span>{title}</span>
+  return (
+    <button className="buttonSwap" onClick={() => clickHandel(url)}>
+      <span>{title}</span>
     </button>
-}
+  );
+};
 
-export default Button
-
+export default Button;
